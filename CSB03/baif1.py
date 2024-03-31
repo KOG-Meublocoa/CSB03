d62c80e56d3cfc2deadee46d7312e1cf3dcceade
KOG-Meublocoa <duongdangkhoa071@gmail.com>
2024-03-31 11:56:23 +0700

    buoi1

---
+++ CSB03/baif1.py
@@ -0,0 +1,60 @@
+# print("Hello,World")
+
+# if 5 > 2:
+#     print("Five is greater than two!")
+
+# if 5 > 2:
+# print("Five is greater than two!") sẽ bị lỗi
+
+# x = 3
+# y = "rô"
+# print("x")
+# print(type(y))
+
+# a = 1
+# name = str("me")
+
+# from turtle import *
+# pen 
+# speed(100)
+# forward(100)
+# right(90)
+# forward(100)
+# right(90)
+# forward(100)
+# right(90)
+# forward(100)
+# mainloop()
+# phải có mainloop để giữ trang vẽ
+
+# from turtle import *
+# pen 
+# color("green")
+# right(72)
+# forward(100)
+# right(72)
+# forward(100)
+# right(72)
+# forward(100)
+# right(72)
+# forward(100)
+# right(72)
+# forward(100)
+# mainloop()
+
+
+from turtle import *
+pen
+speed(100)
+color("green")
+left(144)
+forward(300)
+right(144)
+forward(300)
+right(144)
+forward(300)
+right(144)
+forward(300)
+right(144)
+forward(300)
+mainloop()
