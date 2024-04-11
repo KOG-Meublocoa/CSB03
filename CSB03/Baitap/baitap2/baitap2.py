b6e60d0b2ab43de4fa4819ae21407b87bf828702
KOG-Meublocoa <duongdangkhoa071@gmail.com>
2024-04-11 19:44:26 +0700

    baitap2

---
+++ CSB03/Baitap/baitap2/baitap2.py
@@ -0,0 +1,77 @@
+# bai1
+# radius = float(input("Radius: "))
+# pi = float(3.1416)
+# perimeter = 2*pi*radius
+# area = pi*(radius**2)
+# print(perimeter,"\n",area)
+
+# bai2
+# radius = float(input("Radius: "))
+# pi = float(3.1416)
+# perimeter = 2*pi*radius
+# area = pi*(radius**2)
+# print(perimeter,"\n",area)
+
+# bai3
+# acn = input("Account name: ")
+# domn = input("Domain name: ")
+# print("Full email: ",acn+domn)
+
+# bai4
+# dated = input("Date in MM/DD/YYYY format: ")
+# mang = dated.split("/")
+# print(f'Date in DD/MM/YYYY format: {mang[1]}/{mang[0]}/{mang[2]}')
+
+# bai5
+# B = int(input("Deposit: "))
+# for i in range(5):
+#     print(f"Account after {i} year: ", B*((105/100)**i))
+
+# bai6
+# from turtle import*
+# pen 
+# speed(50)
+# forward(300)
+# left(90)
+# forward(180)
+# left(90)
+# forward(300)
+# left(90)
+# forward(180)
+# left(180)
+# pencolor('#de5246')
+# pensize(15)
+# forward(180)
+# right(120)
+# forward(180)
+# left(62)
+# forward(170)
+# right(122)
+# forward(180)
+# mainloop()
+
+# bai7
+# from turtle import *
+# pensize(7)
+# pencolor('#cf8f03')
+# right(29)
+# backward(150)
+# left(60)
+# forward(150)
+# right(60)
+# forward(150)
+# left(60)
+# backward(150)
+# forward(20)
+# pencolor('#0b2c3c')
+# left(120)
+# forward(140)
+# left(60)
+# backward(150)
+# left(120)
+# forward(160)
+# right(120)
+# forward(151)
+# right(60)
+# forward(30)
+# mainloop()
