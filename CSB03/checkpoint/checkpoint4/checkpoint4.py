18f450a4dd0f671427d70abdc8e84bf700146bd2
KOG-Meublocoa <duongdangkhoa071@gmail.com>
2024-06-09 11:51:17 +0700

    hi

---
+++ CSB03/checkpoint/checkpoint4/checkpoint4.py
@@ -0,0 +1,143 @@
+# phan 1
+# cau 1
+# thisdict = {
+# "HP":20,
+# "DELL":50,
+# "MACBOOK":12,
+# "ASUS":30,
+# }
+# print(thisdict)
+
+# # cau 2 
+# thisdict = {
+# "HP":20,
+# "DELL":50,
+# "MACBOOK":12,
+# "ASUS":30,
+# }
+# print(thisdict["MACBOOK"])
+
+# # cau 3
+# name =input("nhap thiet bi: ")
+# print(thisdict[name])
+
+# phan2
+# cau 1
+# thisdict["TOSHIBA"]=10
+# print(thisdict)
+
+# cau 2
+# a = input("Nhap thiet bi: ")
+# print(thisdict[a])
+
+# cau 3
+# thisdict["DELL"] += 10
+# thisdict["MACBOOK"] -= 10
+# print(thisdict)
+
+# cau 4
+# thisdict = {
+# "HP":20,
+# "DELL":50,
+# "MACBOOK":12,
+# "ASUS":30,
+# }
+# a=(20+50+12+30)
+# print(a)  
+
+# phan 3
+# cau 1
+# thisdict = {
+# "HP":600,
+# "DELL":650,
+# "MACBOOK":1200,
+# "ASUS":400,
+# }
+# print(thisdict)
+
+# # cau 2
+# print(thisdict["ASUS"])
+
+# # cau 3
+# a = input("Nhap thiet bi: ")
+# print(thisdict[a])
+
+# phan 4
+# cau 1
+# thisdict = {
+# "HP":600,
+# "DELL":650,
+# "MACBOOK":1200,
+# "ASUS":400,
+# }
+# print(thisdict)
+# i = "ASUS"
+# i =400 * 5
+# print(i)
+
+# cau 2
+# a = input("Nhap thiet bi: ")
+# a = input("Nhap so can mua: ")
+# a = 
+# print(a)
+
+# cau 3
+
+# phan 5
+# cau 1
+# thisdict = {
+# "HP":20,
+# "DELL":50,
+# "MACBOOK":12,
+# "ASUS":30,
+# }
+# print(thisdict)
+# thisdict = {
+# "HP":600,
+# "DELL":650,
+# "MACBOOK":1200, 
+# "ASUS":400,
+# }
+# print(thisdict)
+
+# i = "HP"(20*600)
+# print(i)
+
+# cau 2
+
+# phan 6
+# cau 1
+# character = {
+#     "Name": "Light",
+#     "Age": 17,
+#     "Strength": 8,
+#     "Defense": 10,
+#     "HP": 100,
+#     "Backpack": ["Shield", "Bread Loaf"],
+#     "Gold": 100,
+#     "Level": 2
+# }
+# print(character)
+
+# # cau 2
+# character["Gold"] += 50
+# print(character)
+
+# # cau 3
+# character["Backpack"].append("FlintStone")
+# print(character)
+
+# phan 7
+# cau 1
+skills = [  
+    {"Skill 1" : "Tackle", "Minimum level": 1, "Damage": 5, "Hit rate": 0.3},
+    {"Skill 2" :"Quick Attack", "Minimum level": 2, "Damage": 3, "Hit rate": 0.5},
+    {"Skill 3 ": "Strong Kick", "Minimum level": 4, "Damage": 9, "Hit rate": 0.2},
+]
+
+# cau 2
+for skill in skills:
+    print(skill["Name"])
+
+
+
