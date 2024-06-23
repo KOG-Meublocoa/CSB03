16123ea7e4d9f8e9bee9a60496bdc6ba9acdb644
KOG-Meublocoa <duongdangkhoa071@gmail.com>
2024-06-23 08:49:32 +0700

    hi

---
+++ CSB03/Bài trên lớp/bai 11/bai11.py
@@ -0,0 +1,51 @@
+# import os
+# f=open('csb03.txt','w')
+# f.write("1234")
+# f.close()
+
+# f=open('csb03.txt',)
+# print(f.read())
+
+
+# if os.path.exists("csb03.txt"):
+#     print('file exists')
+# else:
+#     print("file does not exists")
+# os.remove('csb03.txt')
+
+# with  open('csb03.txt','w') as f:
+#      print(f.read())
+
+
+
+# bai tap
+# cau 1
+# f=open('names.txt','r')
+# f.write('Nikki Roysden')
+# f.write('Mervin Friedland')
+# f.write('Aron Wilkins')
+# f.close()
+# f.=open('names.txt,'r')
+# print(f.read())
+# print("List of name :")
+# for x in f:
+#     print (x)
+
+# cau 3
+# f=open('dangkhoa.txt','a')
+# f.write("Ten":"Dang khoa")
+# f.write("Lop":"11")
+# f.write("Truong":"Ly thai to")
+# f.close()
+# f=open("dangkhoa.txt")
+# print(f.read())
+
+# cau 2
+# import os
+# filename = input("Enter a filename: ")
+
+# if os.path.exists(filename):
+#     with open(filename, 'r') as f:
+#         print(f.read())
+# else:
+#     print("File not found.")
